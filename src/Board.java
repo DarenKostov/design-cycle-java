e5a6e16962c93186b10f3207290f025f8ab71732
DarenKostov <daren.kostov@outlook.com>
2023-05-18 11:49:28 -0700

    comment headers

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,18 @@
-class Board{
-    public static void main(String[] args){
+/*
 
-    }
+    Daren Kostov, Parker Babst
+
+    This class controls the board from image loading to slice swapping
+    
+*/
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+
+class Board extends JPanel{
+  public void paintComponent(Graphics g){
+      super.paintComponent(g);
+  }
 }
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,5 +1,32 @@
-class Puzzle{
+/*
+
+    Daren Kostov, Parker Babst
+
+    This class controls the game and houses the Board and all buttons
+    
+*/
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Container;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+class Puzzle implements ActionListener{
     public static void main(String[] args){
 
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
     }
 }
