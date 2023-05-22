7084b5521541e87b9da34651aedccbc8e0d6b742
ParkerSunset2023 <134004988+ParkerSunset2023@users.noreply.github.com>
2023-05-22 12:03:22 -0700

    Coords added Puzzle.java
    ^

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,9 +1,11 @@
+
+
 /*
 
-    Daren Kostov, Parker Babst
+Daren Kostov, Parker Babst
 
-    This class controls the game and houses the Board and all buttons
-    
+This class controls the game and houses the Board and all buttons
+
 */
 
 
@@ -28,99 +30,120 @@ import java.awt.event.MouseEvent;
 
 class Puzzle implements ActionListener, MouseListener{
 
-    Board board =new Board();
-    // JPanel board =new JPanel();
-    JFrame frame= new JFrame("Puzzle!");
+Board board =new Board();
+// JPanel board =new JPanel();
+JFrame frame= new JFrame("Puzzle!");
+
+JTextField imagePath= new JTextField("Image Path");
+JButton loadImage= new JButton("Load Image");
+JTextField slicesPerSideField= new JTextField("Slices per Side");
+JButton shuffleImageSlices= new JButton("Scramble");
+JLabel correctPositions= new JLabel("Correct Positions: ???");
+
+Container south= new Container();
+int image1X;
+int image1y;
+
+public Puzzle(){
+
+
     
-    JTextField imagePath= new JTextField("Image Path");
-    JButton loadImage= new JButton("Load Image");
-    JTextField slicesPerSideField= new JTextField("Slices per Side");
-    JButton shuffleImageSlices= new JButton("Scramble");
-    JLabel correctPositions= new JLabel("Correct Positions: ???");
+    frame.setSize(700, 727);
+    frame.setResizable(false);
     
-    Container south= new Container();
+    frame.setLayout(new BorderLayout());
+    frame.add(board, BorderLayout.CENTER);
 
+
+    //buttons and text field setup
+    south.setLayout(new GridLayout(1, 3));
+    south.add(imagePath);
+    // imagePath.addActionListener(this);
+    south.add(loadImage);
+    loadImage.addActionListener(this);
+    south.add(slicesPerSideField);
+    south.add(shuffleImageSlices);
+    shuffleImageSlices.addActionListener(this);
+    south.add(correctPositions);
+    frame.add(south, BorderLayout.SOUTH);
     
-    public Puzzle(){
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    
+
+}
+
+public static void main(String[] args){
+    new Puzzle();
+}
 
 
-        
-        frame.setSize(700, 727);
-        frame.setResizable(false);
-        
-        frame.setLayout(new BorderLayout());
-        frame.add(board, BorderLayout.CENTER);
+@Override
+public void actionPerformed(ActionEvent event){
+    if(event.getSource().equals(loadImage)){
 
-
-        //buttons and text field setup
-        south.setLayout(new GridLayout(1, 3));
-        south.add(imagePath);
-        // imagePath.addActionListener(this);
-        south.add(loadImage);
-        loadImage.addActionListener(this);
-        south.add(slicesPerSideField);
-        south.add(shuffleImageSlices);
-        shuffleImageSlices.addActionListener(this);
-        south.add(correctPositions);
-        frame.add(south, BorderLayout.SOUTH);
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-    }
-
-    public static void main(String[] args){
-        new Puzzle();
-    }
-
-
-    @Override
-    public void actionPerformed(ActionEvent event){
-        if(event.getSource().equals(loadImage)){
-
-            int slicesPerSide=3;
-            try{
-                slicesPerSide=Integer.parseInt(slicesPerSideField.getText());
-            }catch(NumberFormatException ex){
-                JOptionPane.showMessageDialog(frame, "Put in slices per side next time, must be in integer. Defaulting to 3.");
-            }
-
-
-
-            
-            if(board.loadImage(imagePath.getText(), slicesPerSide)){
-                correctPositions.setText("Correct Positions: "+board.getCount());
-                frame.repaint();
-                JOptionPane.showMessageDialog(frame, "Loaded image successfully");
-            }else
-                JOptionPane.showMessageDialog(frame, "Loading image failed. Wrong file path?");
-
-            
-        }else if(event.getSource().equals(shuffleImageSlices)){
-            board.scrableSlices();
-            correctPositions.setText("Correct Positions: "+board.getCount());
-            frame.repaint();
+        int slicesPerSide=3;
+        try{
+            slicesPerSide=Integer.parseInt(slicesPerSideField.getText());
+        }catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(frame, "Put in slices per side next time, must be in integer. Defaulting to 3.");
         }
 
-    
+
+
+        
+        if(board.loadImage(imagePath.getText(), slicesPerSide)){
+            frame.repaint();
+            JOptionPane.showMessageDialog(frame, "Loaded image successfully");
+        }else
+            JOptionPane.showMessageDialog(frame, "Loading image failed. Wrong file path?");
+
+
+        
+    }else if(event.getSource().equals(shuffleImageSlices)){
+        board.scrableSlices();
+        frame.repaint();
     }
 
-    
-    @Override
-    public void mouseClicked(MouseEvent event){
-    }
-    @Override
-    public void mouseEntered(MouseEvent event){
-    }
 
-    @Override
-    public void mouseExited(MouseEvent event){
-    }
-    @Override
-    public void mousePressed(MouseEvent event){
-    }
-    public void mouseReleased(MouseEvent event){
-    }
+}
+
+
+@Override
+public void mouseClicked(MouseEvent event){
+}
+@Override
+public void mouseEntered(MouseEvent event){
+}
+
+@Override
+public void mouseExited(MouseEvent event){
+}
+@Override
+public void mousePressed(MouseEvent e){
+	image1X = e.getX();
+	image1y = e.getY();
+
+	
+
+}
+public void mouseReleased(MouseEvent e){
+    //similar to the edge creation, click on 2 images and flip them (instead of drawing a line) 
+	int image2X = e.getX();
+	int image2Y = e.getY();
+	
+	
+	if (board.swapCoords(image1X,image1y,image2X,image2Y) == true) {
+		//switch the image
+			if (allMatch == true) {
+				JOptionPane.showMessageDialog(frame, "You win!");
+			}
+	}
+	else {
+		 JOptionPane.showMessageDialog(frame, "no image clicked");
+	}
+
+}
 
 
 
